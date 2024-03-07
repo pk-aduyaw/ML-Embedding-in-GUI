09652aea0b7abfbf3c2e0e32a4d2e7db4d99470c
Prince K A Aduyaw <princeekow4@gmail.com>
2024-03-07 08:36:07 +0000

    Correct make prediction function

--- pages/2_Predict.py
+++ pages/2_Predict.py
@@ -127,21 +127,53 @@ if 'prediction_proba' not in st.session_state:
 
 # ------- Create a function to make prediction
 def make_prediction(model, encoder):
-    df = pd.DataFrame(st.session_state['df'])
+
+    customerID = st.session_state['customer_id']
+    gender = st.session_state['gender']
+    SeniorCitizen = st.session_state['senior_citizen']
+    Partner = st.session_state['partners']
+    Dependents = st.session_state['dependents']
+    tenure = st.session_state['tenure']
+    PhoneService = st.session_state['phone_service']
+    MultipleLines = st.session_state['multiple_lines']
+    InternetService = st.session_state['internet_service']
+    OnlineSecurity = st.session_state['online_security']
+    OnlineBackup = st.session_state['online_backup']
+    DeviceProtection = st.session_state['device_protection']
+    TechSupport = st.session_state['tech_support']
+    StreamingTV = st.session_state['streaming_tv']
+    StreamingMovies = st.session_state['streaming_movies']
+    Contract = st.session_state['contract']
+    PaperlessBilling = st.session_state['paperless_billing']
+    PaymentMethod = st.session_state['payment_method']
+    MonthlyCharges = st.session_state['monthly_charges']
+    TotalCharges = st.session_state['total_charges']
     
-    # -------- Get the value for predicted
-    prediction = model.predict(df)
+    columns = ['customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
+                'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
+                'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
+                'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
+    
+    values = [[customerID, gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService,
+            MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,
+            TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling,
+            PaymentMethod, MonthlyCharges, TotalCharges]]
+    
+    data = pd.DataFrame(values, columns=columns)
+
+    # -------- Get the value for prediction
+    prediction = model.predict(data)
     prediction = encoder.inverse_transform(prediction)
     st.session_state['prediction'] = prediction
 
     # -------- Get the value for prediction probability
-    prediction_proba = model.predict_proba(df)
+    prediction_proba = model.predict_proba(data)
     st.session_state['prediction_proba'] = prediction_proba
 
-    df['Churn'] = prediction
-    df['Model'] = model_option
+    data['Churn'] = prediction
+    data['Model'] = model_option
 
-    df.to_csv('./data/history.csv', mode='a', header=not os.path.exists('./data/history.csv'), index=False)
+    data.to_csv('./data/history.csv', mode='a', header=not os.path.exists('./data/history.csv'), index=False)
 
     return prediction, prediction_proba
 
@@ -149,70 +181,46 @@ def make_prediction(model, encoder):
 # ------- Prediction page creation
 def input_features():
 
-    with st.form('features', clear_on_submit=True):
+    with st.form('features'):
         model_pipeline, encoder = select_model()
         col1, col2 = st.columns(2)
 
         # ------ Collect customer information
         with col1:
             st.subheader('Demographics')
-            customer_id = st.text_input('Customer ID', placeholder='eg. 1234-ABCDE', key='customer_id')
-            gender = st.radio('Gender', options=['Male', 'Female'],horizontal=True, key='gender')
-            partners = st.radio('Partners', options=['Yes', 'No'],horizontal=True, key='partners')
-            dependents = st.radio('Dependents', options=['Yes', 'No'],horizontal=True, key='dependents')
-            senior_citizen = st.radio("Senior Citizen ('Yes-1, No-0')", options=[1, 0],horizontal=True, key='senior_citizen')
+            st.text_input('Customer ID', value="", placeholder='eg. 1234-ABCDE', key='customer_id')
+            st.radio('Gender', options=['Male', 'Female'],horizontal=True, key='gender',)
+            st.radio('Partners', options=['Yes', 'No'],horizontal=True, key='partners')
+            st.radio('Dependents', options=['Yes', 'No'],horizontal=True, key='dependents')
+            st.radio("Senior Citizen ('Yes-1, No-0')", options=[1, 0],horizontal=True, key='senior_citizen')
         
         # ------ Collect customer account information
         with col1:
             st.subheader('Customer Account Info.')
-            tenure = st.number_input('Tenure', min_value=0, max_value=70, key='tenure')
-            contract = st.selectbox('Contract', options=['Month-to-month', 'One year', 'Two year'], key='contract')
-            payment_method = st.selectbox('Payment Method',
+            st.number_input('Tenure', min_value=0, max_value=70, key='tenure')
+            st.selectbox('Contract', options=['Month-to-month', 'One year', 'Two year'], key='contract')
+            st.selectbox('Payment Method',
                                         options=['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'],
                                         key='payment_method')
-            paperless_billing = st.radio('Paperless Billing', ['Yes', 'No'], horizontal=True, key='paperless_billing')
-            monthly_charges = st.number_input('Monthly Charges', placeholder='Enter amount...', key='monthly_charges')
-            total_charges = st.number_input('Total Charges', placeholder='Enter amount...', key='total_charges')
+            st.radio('Paperless Billing', ['Yes', 'No'], horizontal=True, key='paperless_billing')
+            st.number_input('Monthly Charges', placeholder='Enter amount...', key='monthly_charges')
+            st.number_input('Total Charges', placeholder='Enter amount...', key='total_charges')
             
         # ------ Collect customer subscription information
         with col2:
             st.subheader('Subscriptions')
-            phone_service = st.radio('Phone Service', ['Yes', 'No'], horizontal=True, key='phone_service')
-            multiple_lines = st.selectbox('Multiple Lines', ['Yes', 'No', 'No internet servie'], key='multiple_lines')
-            internet_service = st.selectbox('Internet Service', ['DSL','Fiber optic', 'No'], key='internet_service')
-            online_security = st.selectbox('Online Security', ['Yes', 'No', 'No internet servie'], key='online_security')
-            online_backup = st.selectbox('Online Backup', ['Yes', 'No', 'No internet servie'], key='online_backup')
-            device_protection = st.selectbox('Device Protection', ['Yes', 'No', 'No internet servie'], key='device_protection')
-            tech_support = st.selectbox('Tech Support', ['Yes', 'No', 'No internet servie'], key='tech_support')
-            streaming_tv = st.selectbox('Streaming TV', ['Yes', 'No', 'No internet servie'], key='streaming_tv')
-            streaming_movies = st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet servie'], key='streaming_movies')
+            st.radio('Phone Service', ['Yes', 'No'], horizontal=True, key='phone_service')
+            st.selectbox('Multiple Lines', ['Yes', 'No', 'No internet servie'], key='multiple_lines')
+            st.selectbox('Internet Service', ['DSL','Fiber optic', 'No'], key='internet_service')
+            st.selectbox('Online Security', ['Yes', 'No', 'No internet servie'], key='online_security')
+            st.selectbox('Online Backup', ['Yes', 'No', 'No internet servie'], key='online_backup')
+            st.selectbox('Device Protection', ['Yes', 'No', 'No internet servie'], key='device_protection')
+            st.selectbox('Tech Support', ['Yes', 'No', 'No internet servie'], key='tech_support')
+            st.selectbox('Streaming TV', ['Yes', 'No', 'No internet servie'], key='streaming_tv')
+            st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet servie'], key='streaming_movies')
 
-        # Create a DataFrame with the input features
-        input_data = pd.DataFrame({
-                'customerID': [customer_id],
-                'gender': [gender],
-                'SeniorCitizen': [senior_citizen],
-                'Partner': [partners],
-                'Dependents': [dependents],
-                'tenure': [tenure],
-                'PhoneService': [phone_service],
-                'MultipleLines': [multiple_lines],
-                'InternetService': [internet_service],
-                'OnlineSecurity': [online_security],
-                'OnlineBackup': [online_backup],
-                'DeviceProtection': [device_protection],
-                'TechSupport': [tech_support],
-                'StreamingTV': [streaming_tv],
-                'StreamingMovies': [streaming_movies],
-                'Contract': [contract],
-                'PaperlessBilling': [paperless_billing],
-                'PaymentMethod': [payment_method],
-                'MonthlyCharges': [monthly_charges],
-                'TotalCharges': [total_charges]
-            })
-        st.session_state['df'] = input_data
         st.form_submit_button('Predict', on_click=make_prediction, kwargs=dict(model=model_pipeline, encoder=encoder))
-    return input_data
+    # return input_data
 
 if __name__ == '__main__':
     input_features()
@@ -242,4 +250,3 @@ if __name__ == '__main__':
             col1, col2, col3 = st.columns([.3,.4,.3])
             with col2:
                 st.success('Churn status predicted successfullly🎉')
-
